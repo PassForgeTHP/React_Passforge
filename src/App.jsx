a52ed3a6f2020bbe49fe45d50e106dd24c3d6543
RosaBen <benchabane.rosa@outlook.com>
2025-10-08 13:45:41 +0200

    css+bootstrap for navvbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home'
-import './styles/App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import "./styles/App.css";
 
 function App() {
-  
-
   return (
     <>
-    <header>
-      <Navbar/>
-    </header>
-    <main>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </main>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,73 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
-export default function Navbar(){
-  let navigate = useNavigate();
+export default function Navbar() {
+  // let navigate = useNavigate();
   return (
-    <nav>
-      <div className='container'>
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-bs-toggle='collapse'
-            data-bs-target='#navbarNav'
-            aria-controls='navbarNav'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse' id='navbarNav'>
-
+    <nav className="navbar navbar-expand-lg ">
+      <div className="img-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="pages-container">
+            <div className="container-left">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/security-advice" className="nav-link">
+                    Security Advice
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/pricing" className="nav-link">
+                    Pricing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="container-right">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <Link to="/profile" className="nav-link">
+                    Profile
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/logout" className="nav-link">
+                    Logout
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
+        </div>
       </div>
     </nav>
-  )
+  );
 }
