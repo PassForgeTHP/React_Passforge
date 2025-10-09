18c458038983da79fd0ab725a3c20ded47020e37
melody-ste <melody_stephan@icloud.com>
2025-10-09 22:44:43 +0200

    adding register function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,67 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
 function Register() {
+  const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: { email, password, password_confirmation: passwordConfirmation }
+      }),
+    });
+
+    if (!res.ok) {
+      console.error("Signup failed");
+      return;
+    }
+
+    const data = await res.json();
+    const token = res.headers.get("Authorization")?.split(" ")[1];
+
+    login(data.user, token);
+  };
+
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="container">
+      <div>
+        <h1>Sign Up</h1>
+      </div>
+      
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email :</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <label htmlFor="password">Mot de passe :</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Mot de passe"
+          />
+          <label htmlFor="confirmation">Confirmation :</label>
+          <input
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            type="password"
+            placeholder="Confirmation"
+          />
+          <div>
+            <button type="submit">S'inscrire</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
