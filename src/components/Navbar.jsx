dcfa08ddbe773f0ccaec7554bec73bc700aed74e
RosaBen <benchabane.rosa@outlook.com>
2025-10-09 03:19:37 +0200

    edited to have dropdown menu for Home section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { initializeNavbar } from "../javascript/navbar";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/logo.svg";
 import { useEffect } from "react";
 
 export default function Navbar() {
