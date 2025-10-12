e0377016cc62c1e83d59dd520ec8de80c3b5d726
melody-ste <melody_stephan@icloud.com>
2025-10-12 18:30:57 +0200

    adding Reset password const

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,72 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const ResetPassword = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const token = searchParams.get("token");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/users/password", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          user: {
+            reset_password_token: token,
+            password,
+            password_confirmation: passwordConfirmation,
+          },
+        }),
+      });
+
+      if (response.ok) {
+        setMessage("Password successfully changed!");
+        setTimeout(() => navigate("/login"), 1500);
+      } else {
+        setMessage("Error: please check your link or passwords.");
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage("An error has occurred.");
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="form-card">
+        <h1>Reset your password</h1>
+
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="password"
+              placeholder="New password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            />
+
+            <div className="form-actions">
+              <button type="submit">Reset password</button>
+            </div>
+          </form>
+          {message && <p>{message}</p>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResetPassword;
