a0149145635f53c91601d46b5b1431c0efacbd33
Paarroo <code_source_0@protonmail.com>
2025-10-13 23:10:30 +0200

    Connect to vaultStore and get passwords

--- src/components/PasswordList.jsx
+++ src/components/PasswordList.jsx
@@ -1,7 +1,12 @@
+import useVaultStore from '../stores/vaultStore'
+
 function PasswordList() {
+  const passwords = useVaultStore((state) => state.passwords)
+
   return (
     <div className="container">
       {/* Password list content will be added */}
+      <p style={{ color: 'var(--light)' }}>Total passwords: {passwords.length}</p>
     </div>
   )
 }
