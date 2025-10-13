de617ccb01f49fa4577f57b3129059e450867fa5
Paarroo <code_source_0@protonmail.com>
2025-10-13 23:11:39 +0200

    Add PasswordItem rendering with map

--- src/components/PasswordList.jsx
+++ src/components/PasswordList.jsx
@@ -1,12 +1,19 @@
 import useVaultStore from '../stores/vaultStore'
+import PasswordItem from './PasswordItem'
 
 function PasswordList() {
   const passwords = useVaultStore((state) => state.passwords)
 
   return (
     <div className="container">
-      {/* Password list content will be added */}
-      <p style={{ color: 'var(--light)' }}>Total passwords: {passwords.length}</p>
+      <h2 style={{ color: 'var(--light)', marginBottom: '20px' }}>
+        My Passwords ({passwords.length})
+      </h2>
+      <div>
+        {passwords.map((password) => (
+          <PasswordItem key={password.id} password={password} />
+        ))}
+      </div>
     </div>
   )
 }
