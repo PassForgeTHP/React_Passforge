1c2e1c62ab6a37100252954940e575cebe66e5fa
Paarroo <code_source_0@protonmail.com>
2025-10-13 22:56:43 +0200

    Create PasswordItem component skeleton

---
+++ src/components/PasswordItem.jsx
@@ -0,0 +1,9 @@
+function PasswordItem({ password }) {
+  return (
+    <div className="password-item">
+      {/* Password item content will be added */}
+    </div>
+  )
+}
+
+export default PasswordItem
