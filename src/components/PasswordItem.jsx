012eaeb25e7d1ec4bf5e13d7e547f70f3afaa664
Paarroo <code_source_0@protonmail.com>
2025-10-13 22:59:02 +0200

    Add title and basic styling to PasswordItem

--- src/components/PasswordItem.jsx
+++ src/components/PasswordItem.jsx
@@ -1,7 +1,26 @@
 function PasswordItem({ password }) {
   return (
-    <div className="password-item">
-      {/* Password item content will be added */}
+    <div className="password-item" style={{
+      backgroundColor: 'var(--dark-red)',
+      padding: '16px',
+      marginBottom: '12px',
+      borderRadius: '8px',
+      border: '1px solid var(--base-red)'
+    }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: '8px'
+      }}>
+        <h3 style={{
+          margin: 0,
+          color: 'var(--light)',
+          fontSize: '18px'
+        }}>
+          {password.title}
+        </h3>
+      </div>
     </div>
   )
 }
