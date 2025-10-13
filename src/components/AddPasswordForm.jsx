43033771c1bb64688b769dd7036723d566cfd235
Paarroo <code_source_0@protonmail.com>
2025-10-13 22:30:51 +0200

    Create AddPasswordForm component with basic structure

---
+++ src/components/AddPasswordForm.jsx
@@ -0,0 +1,14 @@
+function AddPasswordForm() {
+  return (
+    <div className="container">
+      <div className="form-card">
+        <h2>Add New Password</h2>
+        <form>
+          {/* Form fields will be added here */}
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AddPasswordForm
