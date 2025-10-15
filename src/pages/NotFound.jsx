cc71e78c1a2600fba253464623cdb0385df8c2d8
melody-ste <melody_stephan@icloud.com>
2025-10-15 20:58:16 +0200

    css to NNot Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="not-found">
-      <h1>404 - Page not found</h1>
-      <p>The page you’re looking for doesn’t exist.</p>
-      <Link to="/">Go back home</Link>
+    <div>
+      <div className="not-found">
+        <h1>404 - Page not found</h1>
+        <p>The page you’re looking for doesn’t exist.</p> 
+        <Link to="/" className="btn-link">Go back home</Link>
+      </div>
     </div>
   );
 }
