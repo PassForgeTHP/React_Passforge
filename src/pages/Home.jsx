93d060476a1f82df806e9a2268fe4c3a07707b60
melody-ste <melody_stephan@icloud.com>
2025-10-16 21:44:52 +0200

    css changes for the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,10 @@ function Home() {
         <h1>PassForge</h1>
         <p>Not your vault, not your secrets</p>
         <div>
-          <Link to="/security-advice" className="home-link">
+          <Link to="/security-advice" className="btn-link">
             Discover
           </Link>
-          <Link to="/register" className="home-link">
+          <Link to="/register" className="btn-link">
             Register
           </Link>
         </div>
